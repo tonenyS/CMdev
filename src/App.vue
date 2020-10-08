98936aa66abda7b9772931e9fa44b9d6d5973412
tonenyS <gundam7784@hotmail.com>
2020-10-08 15:34:37 +0700

    part8

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-<div>
-  <h1>{{title}}</h1>
-  <h2>{{count}}</h2>
-  <button @click="onClickAdd">Add</button>
+  <div>
+    <h1>What is your name ?</h1>
+    <input v-model="message" type="text" placeholder="Key your name">
+     <br>
+     <br>
+     <button @click="message=''">Reset</button>
+     <br>
+     <h1>Click Button</h1>
+     <input v-model="message" type="text" placeholder="my name show"><br>
+     <br>
+     <input v-model="count" type="text">
+     <br><br>
+     <button @click="onClickAdd">click</button> 
+     <button @click="count=0">Reset</button>
+
   </div>
 </template>
 
 <script>
 export default {
- name:"app",
- data(){
-   return{
-           title:"MAHIDON",
-           count: 0
-   }
- },
- methods:{
-   onClickAdd(){
-     this.count=this.count+1
-   }
- }
+  name:"app",
+  data() {
+    return{
+      message:"",
+      count:0
+    }
+  },
+  methods:{
+    onClickAdd(){
+      this.count=this.count+1
+    }
+  }
 };
 </script>
-
+  
 <style>
 
 </style>
