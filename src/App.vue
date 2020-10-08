49df02fbe3027e935054f04426062d17647a3806
tonenyS <gundam7784@hotmail.com>
2020-10-08 16:31:18 +0700

    part9

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,16 @@
 <template>
-  <div>
-    <h1>What is your name ?</h1>
-    <input v-model="message" type="text" placeholder="Key your name">
-     <br>
-     <br>
-     <button @click="message=''">Reset</button>
-     <br>
-     <h1>Click Button</h1>
-     <input v-model="message" type="text" placeholder="my name show"><br>
-     <br>
-     <input v-model="count" type="text">
-     <br><br>
-     <button @click="onClickAdd">click</button> 
-     <button @click="count=0">Reset</button>
-
-  </div>
+<div id="app">
+ <ul>
+      <li><router-link to="/">Home</router-link>  </li>
+      <li><router-link to="/demo1">Demo1</router-link></li>
+</ul>
+     <router-view></router-view>
+</div>
 </template>
 
 <script>
 export default {
   name:"app",
-  data() {
-    return{
-      message:"",
-      count:0
-    }
-  },
-  methods:{
-    onClickAdd(){
-      this.count=this.count+1
-    }
-  }
 };
 </script>
   
---
+++ src/views/demo1.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+<div id="demo1">
+   <h1>What is your name ?</h1>
+    <input v-model="message" type="text" placeholder="Key your name">
+     <br>
+     <br>
+     <button @click="message=''">Reset</button>
+     <br>
+     <h1>Click Button</h1>
+     <input v-model="message" type="text" placeholder="my name show"><br>
+     <br>
+     <input v-model="count" type="text">
+     <br><br>
+     <button @click="onClickAdd">click</button> 
+     <button @click="count=0">Reset</button>
+
+    <ul>
+      <li><router-link to="/">Home</router-link>  </li>
+      <li><router-link to="/demo1">Demo1</router-link></li>
+    </ul>
+     
+     
+  
+  </div>
+</div>
+  
+  
+</template>
+
+
+<script>
+export default {
+    name:"demo1",
+    data() {
+    return{
+      message:"",
+      count:0
+    }
+  },
+  methods:{
+    onClickAdd(){
+      this.count=this.count+1
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
